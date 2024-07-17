941627d27c15f276dd55410845d96582af1da92c
SibaShankar <sibashankartripathy4@gmail.com>
2024-07-17 16:21:53 +0530

    use of setter injection

--- use_of_setter_injection/src/main/java/com/sst/SetterInjection.java
+++ use_of_setter_injection/src/main/java/com/sst/SetterInjection.java
@@ -6,11 +6,11 @@ public class SetterInjection {
 	public Date date;//Instead of date class we can use other class also.
 
 	/* Setter method to set value */
-	public void setDate(Date date) {
+	public void setDate123(Date date) {
 		this.date = date;
 	}
 	
 	public void getCurrentTime() {
-		System.out.println("Hour: "+date.getHours()+" Min: "+date.getMinutes());
+		System.out.println("Current Time: "+date.getHours()+":"+date.getMinutes());
 	}
 }
